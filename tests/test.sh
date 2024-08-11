7c9838a3727491d2acaac590384acd4f6851e89e
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2024-08-11 20:31:28 +0100

    fix: correct option list

--- tests/test.sh
+++ tests/test.sh
@@ -8,9 +8,12 @@ set -e
 #
 
 # all info options, from bash completion file
-all_options="--sysinfo --sysinfo-short --audioinfo --sysinfo-audio --videoinfo --sysinfo-video --netinfo --sysinfo-net --diskinfo --sysinfo-net --dnfinfo --sysinfo-dnf --btrfsinfo --sysinfo-btrfs --pci-verbose --sysinfo-pci-verbose --usb-verbose --sysinfo-usb-verbose"
+all_options="--sysinfo --sysinfo-short --audioinfo --sysinfo-audio --videoinfo --sysinfo-video --netinfo --sysinfo-net --diskinfo --sysinfo-disk --dnfinfo --sysinfo-dnf --btrfsinfo --sysinfo-btrfs --pci-verbose --sysinfo-pci-verbose --usb-verbose --sysinfo-usb-verbose"
 
 for option in $all_options
 do
+    echo "** TESTING option: ${option} **"
     ../fpaste $option --printonly
+    echo
+    echo
 done
